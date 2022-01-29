278b686562392b5507956cc4a236578c1bde2f11
Willem Cazander <git.2022@distributedrebirth.love>
2022-01-29 06:43:06 +0100

    Added naming for fractal number values.

---
+++ core/src/love/distributedrebirth/demo4d/base2t/VŌṁglyphDiamond.java
@@ -0,0 +1,10 @@
+package love.distributedrebirth.demo4d.base2t;
+
+public class VŌṁglyphDiamond {
+
+	// 0th = 1 * V144Tocta (1)
+	// 1th = 1+3 * V144Tocta (4)
+	// 2th = 1+3+6 * V144Tocta (10)
+	// 3th = 1+3+6+14 * V144Tocta (24)
+	// etc
+}
---
+++ core/src/love/distributedrebirth/demo4d/base2t/VŌṁglyphHyperStar.java
@@ -0,0 +1,9 @@
+package love.distributedrebirth.demo4d.base2t;
+
+public class VŌṁglyphHyperStar {
+
+	// 0th = V144Tocta
+	// 1th = 5 * V144Tocta
+	// 2th = 5 * 12 * V144Tocta
+	// etc
+}
