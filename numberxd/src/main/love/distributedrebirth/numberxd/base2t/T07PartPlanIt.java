c9d8459bbe7fb86246509c4a1453c76bcb338a47
Willem Cazander <git.2022@distributedrebirth.love>
2022-02-01 23:39:48 +0100

    Added spaces

--- numberxd/src/main/love/distributedrebirth/numberxd/base2t/T07PartPlanIt.java
+++ numberxd/src/main/love/distributedrebirth/numberxd/base2t/T07PartPlanIt.java
@@ -15,13 +15,13 @@ import love.distributedrebirth.numberxd.base2t.bone.BassBoneStoreKey;
 @BassBoneAlt1Info(name="Fallen sign", website="https://en.wikipedia.org/wiki/Classical_planet#Western_astrology")
 public enum T07PartPlanIt implements BassBoneAlt1<T07PartPlanIt> {
 
-	PART_1("˥","0","♎︎","libra","天秤座"),
-	PART_2("꜉","1","♏︎","scorpio","天蠍座"),
-	PART_3("꜋","2","♓︎","pisces","雙魚座"),
-	PART_4("꜔","3","♍︎","virgo","處女座"),
-	PART_5("꜎","4","♋︎","cancer","癌症"),
+	PART_1("˥","0","♎︎","libra",    "天秤座"),
+	PART_2("꜉","1","♏︎","scorpio",  "天蠍座"),
+	PART_3("꜋","2","♓︎","pisces",   "雙魚座"),
+	PART_4("꜔","3","♍︎","virgo",    "處女座"),
+	PART_5("꜎","4","♋︎","cancer",   "癌症"),
 	PART_6("꜐","5","♑︎","capricorn","摩羯座"),
-	PART_7("˩","7","♈︎","aries","白羊座"),
+	PART_7("˩","7","♈︎","aries",    "白羊座"),
 	;
 	
 	public BassBoneCoffin GET_BBC() { return bbc; }
