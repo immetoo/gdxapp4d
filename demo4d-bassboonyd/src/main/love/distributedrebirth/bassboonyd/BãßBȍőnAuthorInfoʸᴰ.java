fd3ddb464a29398b841cdc4228c8434babcb475e
Willem <git.2022@distributedrebirth.love>
2022-02-05 17:29:41 +0100

    Added javadoc

--- demo4d-bassboonyd/src/main/love/distributedrebirth/bassboonyd/BãßBȍőnAuthorInfoʸᴰ.java
+++ demo4d-bassboonyd/src/main/love/distributedrebirth/bassboonyd/BãßBȍőnAuthorInfoʸᴰ.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Moves copyright into runtime class info.
+ */
 @BãßBȍőnAuthorInfoʸᴰ(name = "willemtsade", copyright = "©Δ∞ 仙上主天")
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE})
