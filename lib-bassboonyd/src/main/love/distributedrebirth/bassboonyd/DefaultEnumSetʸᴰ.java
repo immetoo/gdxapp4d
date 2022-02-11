8790094fdb514aec7696746dc23efe7a52e6b951
Willem <willemtsade.git.2022@distributedrebirth.love>
2022-02-11 17:53:52 +0100

    Cleaned imports

--- lib-bassboonyd/src/main/love/distributedrebirth/bassboonyd/DefaultEnumSetʸᴰ.java
+++ lib-bassboonyd/src/main/love/distributedrebirth/bassboonyd/DefaultEnumSetʸᴰ.java
@@ -1,13 +1,11 @@
 package love.distributedrebirth.bassboonyd;
 
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @BãßBȍőnAuthorInfoʸᴰ(name = "willemtsade", copyright = "©Δ∞ 仙上主天")
 public interface DefaultEnumSetʸᴰ<T> extends DefaultEnumBaseʸᴰ<T> {
